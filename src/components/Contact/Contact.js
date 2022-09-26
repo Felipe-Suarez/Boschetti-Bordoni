54d942f5ece655f38009a2e7fba91c536deaf303
BombenDev <nicolasbomben@gmail.com>
2022-09-26 20:35:33 -0300

    working on contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,38 +1,25 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
-
+//style
+import "./contact.css";
 //falta handleSubmit.
 //logica de envio.
 //estilos.
-const handleSubmit = () => {
-    
-}
-
+const handleSubmit = () => {};
 
 const Contact = () => {
   return (
-    <section className='form-container'>
-
-      <form onSubmit={handleSubmit}>
-        <input
-            type='text'
-            placeholder='Nombre y Apellido'
-        />
-        <input
-            type='email'
-            placeholder='Email'
-        />
-        <textarea
-            placeholder='Mensaje'
-            name='message'
-        />
-        <button type='submit'>Enviar</button>
-    </form>
-
-    </section>
-    
-  )
-}
+      <section className="form-container">
+        <h2 className="contact-title">Contactate con nosotros</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Nombre y Apellido" />
+          <input type="email" placeholder="Email" />
+          <textarea placeholder="Mensaje" name="message" />
+          <button type="submit">Enviar</button>
+        </form>
+      </section>
+  );
+};
 
 export default Contact;
