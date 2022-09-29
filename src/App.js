b25a359406984007ac38933c6aaa88fa381c2819
BombenDev <nicolasbomben@gmail.com>
2022-09-29 20:07:30 -0300

    solving fetch and map in services

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 
 //components
-import Header from './components/header/Header';
-import Home from './components/home/Home';
-import Services from './components/services/Services';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Services from './components/Services/Services';
 import Contact from './components/Contact/Contact'; //nico el nombre de las carpetas en minusculas te parece?
 
 function App() {
--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,27 +1,43 @@
-import './services.css'
-import './mediaQuerys.css'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import Article from './Article'
+//style and query
+import "./services.css";
+import "./mediaQuerys.css";
 
 const Services = () => {
 
-    const [info, setInfo] = useState()
+  const [info, setInfo] = useState([]);
 
-    useEffect(() => {
-        fetch('data/data.json')
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(err => console.log(err))
-    }, [])
+  useEffect(() => {
+    fetch("data/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setInfo(data.servicios);
+      });
+  }, []);
+  
+  //console.log(info);
+  
 
-    return (
+  return(
         <section className="services">
-            <h2 className='services-title'>Servicios</h2>
-            <Article />
+          { 
+            info.map(( service, index) => {
+              return(
+                <article className="services-box" key={index}>
+                  <img className='services-box-img' src='' alt='empresas
+                  familiares' />
+                  <span className='services-box-title'>{service.title}</span>
+                  <p className='services-box-text'>{service.text}</p>
+                </article>
+              )
+            })      
+          }
         </section>
-    )
-}
+  ) 
+};
 
 export default Services;
+
+
