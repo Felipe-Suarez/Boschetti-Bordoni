2acf83388d963302781222f8d939991b155806b7
BombenDev <nicolasbomben@gmail.com>
2022-10-05 19:47:37 -0300

    adding quienes somos

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Services from './components/Services/Services';
+import QuinesSomos from './components/QuienesSomos/QuienesSomos';
 import Nosotros from './components/Nosotros/Nosotros';
 import Contact from './components/Contact/Contact'; //nico el nombre de las carpetas en minusculas te parece?
 import Footer from './components/Footer/Footer';
@@ -14,6 +15,7 @@ function App() {
       <Header />
       <Home />
       <Services />
+      <QuinesSomos/>
       <Nosotros />
       <Contact />
       <Footer />
