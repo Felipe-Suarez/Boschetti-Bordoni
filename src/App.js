b79a33781295ba0008712495afd70921b3db18c5
Felipe Suarez <felipesuarez1525@gmail.com>
2022-10-10 00:24:29 -0300

    animation feature

--- src/App.js
+++ src/App.js
@@ -9,19 +9,47 @@ import QuinesSomos from './components/QuienesSomos/QuienesSomos';
 import Nosotros from './components/Nosotros/Nosotros';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const isMobile = () => {
+    return window.innerWidth < 1000
+  }
+
+  let classDesktop = ''
+  if (!isMobile()) classDesktop = 'animation-desktop';
+
+  const [isLoading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if (isMobile()) {
+      setTimeout(() => {
+        setLoading(false)
+      }, 2000)
+    } else {
+      setLoading(false)
+    }
+  })
+
   return (
     <div className="App">
-      <div className={`load-animation ${window.innerWidth < 1000 && 'animation-mobile'}`}></div>
-      <Header />
-      <Home />
-      <Services />
-      <CallToAction />
-      <QuinesSomos />
-      <Nosotros />
-      <Contact />
-      <Footer />
+      {
+        isLoading ?
+          isMobile() && <div className={`load-animation ${classDesktop}`}></div>
+          :
+          <>
+            <div className={`load-animation ${isMobile() && 'animation-mobile'} ${classDesktop}`}></div>
+            <Header />
+            <Home />
+            <Services />
+            <CallToAction />
+            <QuinesSomos />
+            <Nosotros />
+            <Contact />
+            <Footer />
+          </>
+      }
     </div>
   );
 }
