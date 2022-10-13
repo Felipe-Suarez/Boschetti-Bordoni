3246c30d17ef200df29b9a07ef6416f997c64959
BombenDev <nicolasbomben@gmail.com>
2022-10-13 19:12:51 -0300

    finishing form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,32 +1,46 @@
-import React from "react";
-// import { useState } from "react";
+import { useRef } from "react";
+import emailjs from '@emailjs/browser';
+
 
 //style
 import "./contact.css";
 import "./mediaQuerys.css";
-//falta handleSubmit.
-//logica de envio.
-//estilos.
-const handleSubmit = (e) => {
-  e.preventDefault();
-};
+
 
 const Contact = () => {
+
+  const form = useRef();
+  const serviceId = 'service_j19lei5';
+  const templateId = 'template_42jndtj';
+  const publicKey = 'VTFHGW1HwlxrzSLu4';
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(serviceId, templateId, form.current, publicKey)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
+
   return (
     <section className="form-section">
       
         <h2 className="contact-text">Contactate con nosotros </h2>
         
-          <form onClick={handleSubmit} className="form-container">
+          <form ref={form} onSubmit={sendEmail} className="form-container">
             <div className="form-box">
-            <input type="text" placeholder="Nombre y apellido" />
-            <input type="email" placeholder="Email" />
-            <input type="text" placeholder="Empresa" />
-            <input type="text" placeholder="Industria" />
-            <input type="text" placeholder="Cargo" />
+            <input type="text"  name="user_name" placeholder="Nombre y apellido" required/>
+            <input type="email" name="user_email" placeholder="Email" required/>
+            <input type="text" name="user_business" placeholder="Empresa" required/>
+            <input type="text" name="user_industry" placeholder="Industria" required/>
+            <input type="text" name="user_role" placeholder="Cargo" required/>
             </div>
             <div className="form-box">
-            <textarea type="message" placeholder="Mensaje" />
+            <textarea type="message" name="user_message" placeholder="Mensaje" />
             <button type="submit" className="btn-submit">
               Enviar
             </button>
